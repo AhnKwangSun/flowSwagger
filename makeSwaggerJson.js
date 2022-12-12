5100bde82f08496c5fcc69489384e00cfad65a6c
KwangSun <kwangssab@gmail.com>
2022-12-12 17:37:10 +0900

    [fix] window , mac , linux 무관하게 동작하도록 변경

--- makeSwaggerJson.js
+++ makeSwaggerJson.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const execSync = require("child_process").execSync;
 const YAML = require('json-to-pretty-yaml');
+const cron = require('node-cron');
 
+task = cron.schedule('* * * * *', () => {
 // {
 //     "JSONData": {
 //     "API_KEY": "FLOW_PORTAL_R001",
@@ -14,238 +16,252 @@ const YAML = require('json-to-pretty-yaml');
 // }
 // }
 
-var requestBodyJson = {
-    "description": "",
-    "content": {
-        "application/json": {
-            "schema": {
-                "$ref": "#/components/schemas/input"
-            }
-        }
-    }
-}
-
-var responsesJson = {
-    "200": {
-        "description": "Successful operation",
+    var requestBodyJson = {
+        "description": "",
         "content": {
             "application/json": {
                 "schema": {
-                    "$ref": "#/components/schemas/output"
+                    "$ref": "#/components/schemas/input"
                 }
             }
         }
     }
-}
 
-function recValue(){
-    let data = {
-        "input": {
-            // "required": [],
-            "type": "object",
-            "properties": {}
-        },
-        "output": {
-            "type": "object",
-            "properties": {}
-        },
-    }
-
-    return data;
-}
-
-function recJson(){
-    let data = {
-        "type": "array",
-        "items": {
-            "type": "object",
-            "properties": {
-
-            }
-        }
-    }
-    return data;
-}
-
-function swaggerArray() {
-    let data = {
-        "openapi": "3.0.3",
-        "info": {
-            "title": "FLOW API",
-            "version": "1.0.11"
-        },
-        "paths": {
-        },
-        "components": {
-            "schemas": {}
-        },
-        "servers": [
-            {
-                "url": "https://dualserver.flow.team",
-                "variables": {},
-                "description": "devServer"
-            }
-        ]
-    }
-    return data;
-};
-
-function resultExec(command){
-    let array = execSync(command).toString().split("\n")
-    return array.slice(0,array.length-1);
-}
-
-var folderArray = resultExec("find D:/flow-was/src/WEB-INF/src/jex/studio/domain/act -mtime -5 -type d -and ! -name '*act'");
-var folderObject = {}
-var definitionsJson = [];
-for(let i in folderArray){
-    try{
-        folderObject[folderArray[i]] = resultExec("ls " + folderArray[i] + " | grep -e 'In.java' -e 'Out.java'");
-    } catch {
-        console.log('passed : ' + folderArray[i]);
-    }
-}
-console.log(JSON.stringify(folderObject, null, 2))
-
-let tmpRecValue;
-for (let i in folderArray) {
-    try{
-        let currentFolderFile = folderObject[folderArray[i]]
-        tmpRecValue = recValue();
-        let path = "/" + currentFolderFile[0].substring(1, currentFolderFile[0].length - 7) + ".jct";
-        let pathRec = {
-            [path]: {
-                "post": {}
-            }
-        }
-        let tmpSwaagerArray;
-        tmpSwaagerArray = swaggerArray();
-        tmpSwaagerArray.paths = pathRec;
-        for (let j in currentFolderFile) {
-            const csv = fs.readFileSync(folderArray[i] + "/" + currentFolderFile[j]);
-            const test = csv.toString("utf-8");
-
-            let actionId = test.split('public String get_Id () {');
-            let actionName = test.split('public String get_Name() {');
-            let actionVersion = test.split('public String get_Version () {');
-            let actionCreator = test.split('public String get_Creator () {');
-            let actionIdLength = actionId.length - 1;
-
-            actionId = actionId[actionIdLength].substring(12, actionId[actionIdLength].indexOf('";'));
-            actionName = actionName[actionIdLength].substring(12, actionName[actionIdLength].indexOf('";'));
-            actionVersion = actionVersion[actionIdLength].substring(12, actionVersion[actionIdLength].indexOf('";'));
-            actionCreator = actionCreator[actionIdLength].substring(12, actionCreator[actionIdLength].indexOf('";'));
-
-            tmpSwaagerArray.paths[path].post['tags'] = [actionId];
-            tmpSwaagerArray.paths[path].post['summary'] = actionName
-            tmpSwaagerArray.paths[path].post['description'] = actionName + "\n" + actionCreator + " " + actionVersion;
-
-            const imoJsonParse = test.split('JexDomainUtil.parseIMODataItem(');
-            const jsonArray = [];
-            const arrayLength = imoJsonParse.length - 1;
-            imoJsonParse[arrayLength] = imoJsonParse[arrayLength].substring(imoJsonParse[arrayLength].indexOf('};'))
-
-            for (let i in imoJsonParse) {
-                if (imoJsonParse[i].includes('type_imo')) {
-                    let tmpSubString = imoJsonParse[i].substring(0, imoJsonParse[i].length - 6);
-                    jsonArray.push(JSON.parse(tmpSubString))
-                }
-            }
-
-            if (currentFolderFile[j].includes("In.java")) {
-                for (let k in jsonArray) {
-                    let originParse = JSON.parse(jsonArray[k]);
-                    if (originParse.type_imo !== 'RECORD') {
-                        // if (originParse.required === 'Y') tmpRecValue.input.required.push(originParse.id)
-                        tmpRecValue.input.properties[originParse.id] = {
-                            "type": "string",
-                            "description": originParse.name
-                        }
-                    } else {
-                        tmpRecValue.input.properties[originParse.id] = makeSwaggerJson(originParse, folderArray[i])
-                    }
-                }
-            } else {
-                for (let k in jsonArray) {
-                    let originParse = JSON.parse(jsonArray[k]);
-                    if (originParse.type_imo !== 'RECORD') {
-                        // if (originParse.required === 'Y') tmpRecValue.output.required.push(originParse.id)
-                        tmpRecValue.output.properties[originParse.id] = {
-                            "type": "string",
-                            "description": originParse.name
-                        }
-                    } else {
-                        tmpRecValue.output.properties[originParse.id] = makeSwaggerJson(originParse, folderArray[i])
+    var responsesJson = {
+        "200": {
+            "description": "Successful operation",
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "$ref": "#/components/schemas/output"
                     }
                 }
             }
+        }
+    }
 
+    function recValue(){
+        let data = {
+            "input": {
+                // "required": [],
+                "type": "object",
+                "properties": {}
+            },
+            "output": {
+                "type": "object",
+                "properties": {}
+            },
         }
 
-        tmpSwaagerArray.paths[path].post['requestBody'] = requestBodyJson;
-        tmpSwaagerArray.paths[path].post['responses'] = responsesJson;
-        tmpSwaagerArray.components.schemas = tmpRecValue;
+        return data;
+    }
+
+    function recJson(){
+        let data = {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+
+                }
+            }
+        }
+        return data;
+    }
+
+    function swaggerArray() {
+        let data = {
+            "openapi": "3.0.3",
+            "info": {
+                "title": "FLOW API",
+                "version": "1.0.11"
+            },
+            "paths": {
+            },
+            "components": {
+                "schemas": {}
+            },
+            "servers": [
+                {
+                    "url": "https://develop.flow.team",
+                    "variables": {},
+                    "description": "developServer"
+                },
+                {
+                    "url": "https://staging.flow.team",
+                    "variables": {},
+                    "description": "stagingServer"
+                },
+                {
+                    "url": "https://flowtest.info",
+                    "variables": {},
+                    "description": "testServer"
+                },
+                {
+                    "url": "https://dualserver.flow.team",
+                    "variables": {},
+                    "description": "devServer"
+                }
+            ]
+        }
+        return data;
+    };
+
+    function resultExec(command){
+        let array = execSync(command).toString().split("\n")
+        return array.slice(0,array.length-1);
+    }
+
+    var folderArray = resultExec("find /WAS_DATA/webRoot/flow/src/WEB-INF/src/jex/studio/domain/act -type d -and ! -name '*act'");
+    var folderObject = {}
+    var definitionsJson = [];
+    for(let i in folderArray){
+        try{
+            folderObject[folderArray[i]] = resultExec("ls " + folderArray[i] + " | grep -e 'In.java' -e 'Out.java'");
+        } catch {
+            console.error('Folder ERROR : ' + folderArray[i]);
+        }
+    }
+
+    let tmpRecValue;
+    for (let i in folderArray) {
+        try{
+            let currentFolderFile = folderObject[folderArray[i]]
+            tmpRecValue = recValue();
+            let path = "/" + currentFolderFile[0].substring(1, currentFolderFile[0].length - 7) + ".jct";
+            let pathRec = {
+                [path]: {
+                    "post": {}
+                }
+            }
+            let tmpSwaagerArray;
+            tmpSwaagerArray = swaggerArray();
+            tmpSwaagerArray.paths = pathRec;
+            for (let j in currentFolderFile) {
+                const csv = fs.readFileSync(folderArray[i] + "/" + currentFolderFile[j]);
+                const test = csv.toString("utf-8");
+
+                let actionId = test.split('public String get_Id () {');
+                let actionName = test.split('public String get_Name() {');
+                let actionVersion = test.split('public String get_Version () {');
+                let actionCreator = test.split('public String get_Creator () {');
+                let actionIdLength = actionId.length - 1;
+
+                actionId = actionId[actionIdLength].substring(actionId[actionIdLength].indexOf('"')+1, actionId[actionIdLength].indexOf('";'));
+                actionName = actionName[actionIdLength].substring(actionName[actionIdLength].indexOf('"')+1, actionName[actionIdLength].indexOf('";'));
+                actionVersion = actionVersion[actionIdLength].substring(actionVersion[actionIdLength].indexOf('"')+1, actionVersion[actionIdLength].indexOf('";'));
+                actionCreator = actionCreator[actionIdLength].substring(actionCreator[actionIdLength].indexOf('"')+1, actionCreator[actionIdLength].indexOf('";'));
+
+                tmpSwaagerArray.paths[path].post['tags'] = [actionId];
+                tmpSwaagerArray.paths[path].post['summary'] = actionName
+                tmpSwaagerArray.paths[path].post['description'] = actionName + "\n" + actionCreator + " " + actionVersion;
+
+                const imoJsonParse = test.split('JexDomainUtil.parseIMODataItem(');
+                const jsonArray = [];
+                const arrayLength = imoJsonParse.length - 1;
+                imoJsonParse[arrayLength] = imoJsonParse[arrayLength].substring(0,imoJsonParse[arrayLength].indexOf('};'))
+
+                for (let i in imoJsonParse) {
+                    if (imoJsonParse[i].includes('type_imo')) {
+                        let tmpSubString = imoJsonParse[i].substring(0, imoJsonParse[i].lastIndexOf(')'));
+                        jsonArray.push(JSON.parse(tmpSubString))
+                    }
+                }
+
+                if (currentFolderFile[j].includes("In.java")) {
+                    for (let k in jsonArray) {
+                        let originParse = JSON.parse(jsonArray[k]);
+                        if (originParse.type_imo !== 'RECORD') {
+                            // if (originParse.required === 'Y') tmpRecValue.input.required.push(originParse.id)
+                            tmpRecValue.input.properties[originParse.id] = {
+                                "type": "string",
+                                "description": originParse.name
+                            }
+                        } else {
+                            tmpRecValue.input.properties[originParse.id] = makeSwaggerJson(originParse, folderArray[i])
+                        }
+                    }
+                } else {
+                    for (let k in jsonArray) {
+                        let originParse = JSON.parse(jsonArray[k]);
+                        if (originParse.type_imo !== 'RECORD') {
+                            // if (originParse.required === 'Y') tmpRecValue.output.required.push(originParse.id)
+                            tmpRecValue.output.properties[originParse.id] = {
+                                "type": "string",
+                                "description": originParse.name
+                            }
+                        } else {
+                            tmpRecValue.output.properties[originParse.id] = makeSwaggerJson(originParse, folderArray[i])
+                        }
+                    }
+                }
+
+            }
+
+            tmpSwaagerArray.paths[path].post['requestBody'] = requestBodyJson;
+            tmpSwaagerArray.paths[path].post['responses'] = responsesJson;
+            tmpSwaagerArray.components.schemas = tmpRecValue;
 
 
-        definitionsJson.push({
-            "name" : tmpSwaagerArray.paths[path].post['tags'][0],
-            "url" : "./definitions/" + tmpSwaagerArray.paths[path].post['tags'][0]+".json"
-        })
+            definitionsJson.push({
+                "name" : tmpSwaagerArray.paths[path].post['tags'][0],
+                "url" : "./definitions/" + tmpSwaagerArray.paths[path].post['tags'][0]+".json"
+            })
 
-        fs.writeFile('./public/definitions/'+tmpSwaagerArray.paths[path].post['tags'][0]+".json",JSON.stringify(tmpSwaagerArray, null, 2),function(err){
+            fs.writeFile('./public/definitions/'+tmpSwaagerArray.paths[path].post['tags'][0]+".json",JSON.stringify(tmpSwaagerArray, null, 2),function(err){
+                if (err === null){}
+            });
+        } catch {
+            console.error("Parse ERROR : " , folderArray[i])
+        }
+
+        let sidebarConfig = "export const sidebarConfig = { title: \"FLOW API LIST\", extendTheme: { colors: { brand: { \"50\": \"#E6FFFA\", \"100\": \"#B2F5EA\", \"200\": \"#81E6D9\", \"300\": \"#4FD1C5\", \"400\": \"#38B2AC\", \"500\": \"#319795\", \"600\": \"#2C7A7B\", \"700\": \"#285E61\", \"800\": \"#234E52\", \"900\": \"#1D4044\" }, }, }, };"
+        let swaggerUI = "export const swaggerUIProps = { queryConfigEnabled: true,}; "
+        let definitions = "export const definitions =" + JSON.stringify(definitionsJson, null, 2);
+
+        let writeConfig = sidebarConfig + swaggerUI + definitions
+
+        fs.writeFile('./config.js',writeConfig,function(err){
             if (err === null){}
         });
-    } catch {
-        console.log(folderArray[i])
+
     }
 
-    let sidebarConfig = "export const sidebarConfig = { title: \"FLOW API LIST\", extendTheme: { colors: { brand: { \"50\": \"#E6FFFA\", \"100\": \"#B2F5EA\", \"200\": \"#81E6D9\", \"300\": \"#4FD1C5\", \"400\": \"#38B2AC\", \"500\": \"#319795\", \"600\": \"#2C7A7B\", \"700\": \"#285E61\", \"800\": \"#234E52\", \"900\": \"#1D4044\" }, }, }, };"
-    let swaggerUI = "export const swaggerUIProps = { queryConfigEnabled: true,}; "
-    let definitions = "export const definitions =" + JSON.stringify(definitionsJson, null, 2);
+    function makeSwaggerJson(recursiveArray,folderPath){
 
-    let writeConfig = sidebarConfig + swaggerUI + definitions
+        let tmpRecJson = recJson();
 
-    fs.writeFile('./config.js',writeConfig,function(err){
-        if (err === null){}
-    });
+        let javaFileName = recursiveArray.resource
+        javaFileName = javaFileName.split('.');
+        javaFileName = folderPath + "/" + javaFileName[javaFileName.length-1] + '.java';
+        const csv = fs.readFileSync(javaFileName);
+        const recordCsv = csv.toString("utf-8");
+        const recordCsv2 = recordCsv.split('JexDomainUtil.parseIMODataItem(');
+        const recordJsonArray = [];
+        const recordArrayLength = recordCsv2.length-1;
+        recordCsv2[recordArrayLength] = recordCsv2[recordArrayLength].substring(recordCsv2[recordArrayLength].indexOf('};'))
 
-}
-
-function makeSwaggerJson(recursiveArray,folderPath){
-
-    let tmpRecJson = recJson();
-
-    let javaFileName = recursiveArray.resource
-    javaFileName = javaFileName.split('.');
-    javaFileName = folderPath + "/" + javaFileName[javaFileName.length-1] + '.java';
-    const csv = fs.readFileSync(javaFileName);
-    const recordCsv = csv.toString("utf-8");
-    const recordCsv2 = recordCsv.split('JexDomainUtil.parseIMODataItem(');
-    const recordJsonArray = [];
-    const recordArrayLength = recordCsv2.length-1;
-    recordCsv2[recordArrayLength] = recordCsv2[recordArrayLength].substring(recordCsv2[recordArrayLength].indexOf('};'))
-
-    for(let i in recordCsv2)
-    {
-        if(recordCsv2[i].includes('type_imo')){
-            recordJsonArray.push(JSON.parse(recordCsv2[i].substring(0,recordCsv2[i].length-6)))
-        }
-    }
-
-    for(let i in recordJsonArray){
-        let recursiveArray = JSON.parse(recordJsonArray[i]);
-        if(recursiveArray.type_imo !== 'RECORD'){
-            tmpRecJson.items.properties[recursiveArray.id] = {
-                "type" : "string",
-                "description" : recursiveArray.name
+        for(let i in recordCsv2)
+        {
+            if(recordCsv2[i].includes('type_imo')){
+                recordJsonArray.push(JSON.parse(recordCsv2[i].substring(0,recordCsv2[i].length-5)))
             }
-        } else {
-            tmpRecJson.items.properties[recursiveArray.id] = makeSwaggerJson(recursiveArray,folderPath);
         }
-    }
 
-    return tmpRecJson;
-}
+        for(let i in recordJsonArray){
+            let recursiveArray = JSON.parse(recordJsonArray[i]);
+            if(recursiveArray.type_imo !== 'RECORD'){
+                tmpRecJson.items.properties[recursiveArray.id] = {
+                    "type" : "string",
+                    "description" : recursiveArray.name
+                }
+            } else {
+                tmpRecJson.items.properties[recursiveArray.id] = makeSwaggerJson(recursiveArray,folderPath);
+            }
+        }
+
+        return tmpRecJson;
+    }
 
 // swaggerArray.paths.get['tags'] = 'FLOW_PORTAL_R001';
 // swaggerArray.paths.get['summary'] = '포탈 정보 가져오기';
@@ -255,5 +271,8 @@ function makeSwaggerJson(recursiveArray,folderPath){
 // swaggerArray.paths.get['responses'] = responsesJson;
 
 // console.log(JSON.stringify(swaggerArray,null,2))
+    console.log("finish : " + new Date());
 
+});
 
+task.start();
--- test.js
+++ test.js
@@ -1,192 +1,23 @@
 const fs = require("fs");
-const execSync = require("child_process").execSync;
-const YAML = require('json-to-pretty-yaml');
-var swaggerArray = {
-    "openapi":"3.0.3",
-    "info": {
-        "title": "FLOW API",
-        "version": "1.0.11"
-    },
-    "paths": {
-        "post": {
+const csv = fs.readFileSync('./config123');
+const test = csv.toString("utf-8");
 
-        },
-        "get": {
+// console.log(test);
+//
+// fs.writeFile('./config.js',test,function(err){
+//     if (err === null){}
+// });
 
-        }
-    },
-    "servers": [
-        {
-            "url": "https://dualserver.flow.team",
-            "variables": {},
-            "description": "devServer"
-        }
-    ]
-};
-
-var requestBodyJson = {
-    "description": "포탈 정보 가져오기",
-    "content": {
-        "application/json": {
-            "schema": {
-            }
-        },
-        "application/x-www-form-urlencoded": {
-            "schema": {
-            }
-        }
-    },
-    "required": true
-}
-
-var responsesJson = {
-    "200": {
-        "description": "Successful operation",
-        "content": {
-            "application/json": {
-                "schema": {
-                }
-            }
-        }
+let definitions =[
+    {
+        "name": "ACT_GENERATE_S3_PRESIGNED_URL",
+        "url": "./definitions/ACT_GENERATE_S3_PRESIGNED_URL.json"
     }
-}
+]
 
-var componentsJson = {
-    "schemas": {}
-}
+definitions.push(    {
+    "name": "ACT_GENERATE_S3_PRESIGNED_URL",
+    "url": "./definitions/ACT_GENERATE_S3_PRESIGNED_URL.json"
+})
 
-function recValue(){
-    let data = {
-        "input": {
-            "required": [],
-            "type": "object",
-            "properties": {}
-        },
-        "output": {
-            "required": [],
-            "type": "object",
-            "properties": {}
-        },
-    }
-
-    return data
-}
-
-function resultExec(command){
-    let array = execSync(command).toString().split("\n")
-    return array.slice(0,array.length-1);
-}
-
-var folderArray = resultExec("find D:/flow-was/src/WEB-INF/src/jex/studio/domain/act -mtime -6 -type d -and ! -name '*act'");
-var folderObject = {}
-for(let i in folderArray){
-    folderObject[folderArray[i]] = resultExec("ls " + folderArray[i] + " | grep -e 'In.java' -e 'Out.java'");
-}
-
-let tepRecValue;
-for (let i in folderArray) {
-    let currentFolderFile = folderObject[folderArray[i]]
-    for (let j in currentFolderFile) {
-        componentsJson = {"schemas": {}};
-        tepRecValue = recValue();
-        const csv = fs.readFileSync(folderArray[i] + "/" + currentFolderFile[j]);
-        const test = csv.toString("utf-8");
-
-        const actionId = test.split('public String get_Id () {');
-        const actionName = test.split('public String get_Name () {');
-        const actionVersion = test.split('public String get_Version () {');
-        const actionCreator = test.split('public String get_Creator () {');
-        const imoJsonParse = test.split('JexDomainUtil.parseIMODataItem(');
-        const jsonArray = [];
-        const arrayLength = imoJsonParse.length - 1;
-        imoJsonParse[arrayLength] = imoJsonParse[arrayLength].substring(imoJsonParse[arrayLength].indexOf('};'))
-
-        for (let i in imoJsonParse) {
-            if (imoJsonParse[i].includes('type_imo')) {
-                let tmpSubString = imoJsonParse[i].substring(0, imoJsonParse[i].length - 6);
-                jsonArray.push(JSON.parse(tmpSubString))
-            }
-        }
-
-        for (let k in jsonArray) {
-            let originParse = JSON.parse(jsonArray[k]);
-            if (originParse.type_imo !== 'RECORD') {
-                if (originParse.required === 'Y') tepRecValue.input.required.push(originParse.id)
-                tepRecValue.input.properties[originParse.id] = {
-                    "type": "string",
-                    "description": originParse.name
-                }
-            } else {
-                tepRecValue.input.properties[originParse.id] = makeSwaggerJson(originParse, folderArray[i])
-            }
-        }
-    }
-}
-
-function makeSwaggerJson(recursiveArray,folderPath){
-
-    let tmpRecJson = {
-        "type": "array",
-        "items": {
-            "type": "object",
-            "properties": {
-
-            }
-        }
-    }
-
-    if(recursiveArray.type_imo === 'RECORD'){
-        let javaFileName = recursiveArray.resource
-        javaFileName = javaFileName.split('.');
-        javaFileName = folderPath + "/" + javaFileName[javaFileName.length-1] + '.java';
-        const csv = fs.readFileSync(javaFileName);
-        const recordCsv = csv.toString("utf-8");
-        const recordCsv2 = recordCsv.split('JexDomainUtil.parseIMODataItem(');
-        const recordJsonArray = [];
-        const recordArrayLength = recordCsv2.length-1;
-        recordCsv2[recordArrayLength] = recordCsv2[recordArrayLength].substring(recordCsv2[recordArrayLength].indexOf('};'))
-
-        for(let i in recordCsv2)
-        {
-            if(recordCsv2[i].includes('type_imo')){
-                recordJsonArray.push(JSON.parse(recordCsv2[i].substring(0,recordCsv2[i].length-6)))
-            }
-        }
-
-        for(let i in recordJsonArray){
-            let recursiveArray = JSON.parse(recordJsonArray[i]);
-            if(recursiveArray.type_imo !== 'RECORD'){
-                tmpRecJson.input.properties[recursiveArray.id].items = {
-                    "type" : "string",
-                    "description" : recursiveArray.name
-                }
-            } else {
-                tmpRecJson.input.properties[recursiveArray.id].items = {
-                    "type" : "array",
-                    "description" : recursiveArray.name
-                }
-            }
-            tmpRecJson.input.properties[recursiveArray.id] = makeSwaggerJson(recursiveArray,folderPath,javaFileName.includes("In.java"),recursiveArray.type_imo === 'RECORD',currentRecId)
-        }
-
-        return
-    }
-}
-
-swaggerArray.paths.post['tags'] = 'FLOW_PORTAL_R001';
-swaggerArray.paths.post['summary'] = '포탈 정보 가져오기';
-swaggerArray.paths.post['description'] = '포탈 정보 가져오기';
-swaggerArray.paths.post['operationId'] = '포탈 정보 가져오기';
-swaggerArray.paths.post['requestBody'] = requestBodyJson;
-swaggerArray.paths.post['responses'] = responsesJson;
-
-swaggerArray.paths.get['tags'] = 'FLOW_PORTAL_R001';
-swaggerArray.paths.get['summary'] = '포탈 정보 가져오기';
-swaggerArray.paths.get['description'] = '포탈 정보 가져오기';
-swaggerArray.paths.get['operationId'] = '포탈 정보 가져오기';
-swaggerArray.paths.get['requestBody'] = requestBodyJson;
-swaggerArray.paths.get['responses'] = responsesJson;
-
-console.log(JSON.stringify(swaggerArray,null,2))
-
-
+console.log(definitions)
